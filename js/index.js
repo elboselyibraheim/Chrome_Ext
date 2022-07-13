ee9027ce25a93758cd50b00146c7783f68a7fca5
ibrahim_elbosily <86437157+elboselyibraheim@users.noreply.github.com>
2022-07-13 15:20:17 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
-let myContainer = document.getElementsByClassName("container");
-myContainer.window.size(500, 500);
+let nowPlaying = document.querySelector(".now-playing");
+let trackName = document.querySelector(".track-name");
+let trackArtist = document.querySelector(".track-artist");
+
+let playPauseBtn = document.querySelector(".play-pause-track");
+
+let prevTrack = document.querySelector(".prev-track");
+let nextTrack = document.querySelector(".next-track");
+let repeatTrack = document.querySelector(".repeat-track");
+
+// function to play/pause track
+function playPauseTrack() {
+	
